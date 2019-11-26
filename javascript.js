80cce0e3e2ab9d11cb5955cee016cee1e80f4e25
Kensen <hirohtk@gmail.com>
2019-11-25 18:03:57 -0800

    added bootstrap modals for each assignment

--- javascript.js
+++ javascript.js
@@ -1,21 +1,83 @@
-$(".btn").on("click", function(event) {
-event.preventDefault();
+$(".btn").on("click", function (event) {
+    event.preventDefault();
 
-let specificAssignment = $(this).parent().attr("name");
+    let specificAssignment = $(this).parent().attr("name");
 
-switch (specificAssignment) {
+    switch (specificAssignment) {
 
-    case "friendFinder":
+        case "friendFinder":
 
-    console.log("FriendFinder Assignment Selected");
-    $("#guide-modal").modal();
-    $("#projectTitle").text(specificAssignment);
-    $("#repo").html("<a href='https://github.com/hirohtk/friend-finder'> GitHub Repository </a>")
-    $("#deployed").html("<a href='https://stormy-ocean-54298.herokuapp.com/'> Deployed Application </a>")
+            console.log("FriendFinder Assignment Selected");
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/friend-finder'> GitHub Repository </a>")
+            $("#deployed").html("<a href='https://stormy-ocean-54298.herokuapp.com/'> Deployed Application </a>")
 
-    break;
+            break;
 
-}
+        case "eatDaBurger":
+
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/burger'> GitHub Repository </a>")
+            $("#deployed").html("<a href='    '> Deployed Application </a>")
+
+            break;
+
+        case "bamazon":
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/bamazon_assignment'> GitHub Repository </a>")
+            $("#deployed").html("CLI App - Undeployable")
+
+            break;
+
+        case "wordGuess-adv":
+
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/word_guess_node'> GitHub Repository </a>")
+            $("#deployed").html("CLI App - Undeployable")
+
+            break;
+
+        case "liri":
+
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/liri-node-app'> GitHub Repository </a>")
+            $("#deployed").html("CLI App - Undeployable")
+
+            break;
+
+        case "trivia":
+
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/TriviaGame'> GitHub Repository </a>")
+            $("#deployed").html("<a href='https://hirohtk.github.io/TriviaGame/'> Deployed Application </a>")
+
+            break;
+
+        case "starWars":
+
+            $("#guide-modal").modal();
+            $("#projectTitle").text(specificAssignment);
+            $("#repo").html("<a href='https://github.com/hirohtk/Star-wars-game'> GitHub Repository </a>")
+            $("#deployed").html("<a href='https://hirohtk.github.io/Star-wars-game/'> Deployed Application </a>")
+
+            break;
+
+        case "wordGuess-easy":
+
+        $("#guide-modal").modal();
+        $("#projectTitle").text(specificAssignment);
+        $("#repo").html("<a href='https://github.com/hirohtk/Word-guess-game'> GitHub Repository </a>")
+        $("#deployed").html("<a href='https://hirohtk.github.io/Word-guess-game/'> Deployed Application </a>")
+
+        break;
+        
+    }
 
 
 });
