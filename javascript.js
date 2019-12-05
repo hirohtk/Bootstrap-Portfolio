390811585f68346756dd2ca3b5c1aa448f9949ea
Kensen <hirohtk@gmail.com>
2019-12-04 21:58:57 -0800

    refactored JS file to minimize DRY- was able to reduce DRY for JQuery and hoped to do so in order to not have to do a switch statement, but can't get around that

--- javascript.js
+++ javascript.js
@@ -1,83 +1,102 @@
+const linksList = {
+    eatDaBurger: {
+        repo: "https://github.com/hirohtk/burger",
+        deployed: "https://peaceful-badlands-20443.herokuapp.com/",
+    },
+    friendFinder: {
+        repo: "https://github.com/hirohtk/friend-finder",
+        deployed: "https://stormy-ocean-54298.herokuapp.com/",
+    },
+    bamazon: {
+        repo: "https://github.com/hirohtk/bamazon_assignment",
+        deployed: "CLI App - Undeployable",
+    },
+    wordguessAdv: {
+        repo: "https://github.com/hirohtk/word_guess_node",
+        deployed: "CLI App - Undeployable",
+    },
+    liri: {
+        repo: "https://github.com/hirohtk/liri-node-app",
+        deployed: "CLI App - Undeployable",
+    },
+    trivia: {
+        repo: "https://github.com/hirohtk/TriviaGame",
+        deployed: "https://hirohtk.github.io/TriviaGame/",
+    },
+    starWars: {
+        repo: "https://github.com/hirohtk/Star-wars-game",
+        deployed: "https://hirohtk.github.io/Star-wars-game/",
+    },
+    wordGuessEasy: {
+        repo: "https://github.com/hirohtk/Word-guess-game",
+        deployed: "https://hirohtk.github.io/Word-guess-game/",
+    },
+
+}
+
 $(".btn").on("click", function (event) {
     event.preventDefault();
 
     let specificAssignment = $(this).parent().attr("name");
+    console.log(specificAssignment);
 
-    switch (specificAssignment) {
+    function dry1(specificAssignment) {
+        let repo;
+        let deployed;
+        // Was trying the few lines of code (commented out below), but...
+        // DOES NOT WORK: specificAssignment, although containing the string I need, the object above reads it as specificAssignment
+        // So I still need a switch statement.  Prevented DRY for the DOM JS but not the switch
+        // repo = linksList.specificAssignment.repo;
+        // deployed = linksList.specificAssignment.deployed;
 
-        case "friendFinder":
-
-            console.log("FriendFinder Assignment Selected");
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/friend-finder'> GitHub Repository </a>")
-            $("#deployed").html("<a href='https://stormy-ocean-54298.herokuapp.com/'> Deployed Application </a>")
-
+        switch (specificAssignment) {
+            case "eatDaBurger":
+                repo = linksList.eatDaBurger.repo
+                deployed = linksList.eatDaBurger.deployed
+            break;
+            case "friendFinder":
+                repo = linksList.friendFinder.repo
+                deployed = linksList.friendFinder.deployed
+            break;
+            case "bamazon":
+                repo = linksList.bamazon.repo
+                deployed = linksList.bamazon.deployed
+            break;
+            case "wordguessAdv":
+                repo = linksList.wordguessAdv.repo
+                deployed = linksList.wordguessAdv.deployed
+            break;
+            case "liri":
+                repo = linksList.liri.repo
+                deployed = linksList.liri.deployed
+            break;
+            case "trivia":
+                repo = linksList.trivia.repo
+                deployed = linksList.trivia.deployed
+            break;
+            case "starWars":
+                repo = linksList.starWars.repo
+                deployed = linksList.starWars.deployed
+            break;
+            case "wordGuessEasy":
+                repo = linksList.wordGuessEasy.repo
+                deployed = linksList.wordGuessEasy.deployed
             break;
 
-        case "eatDaBurger":
-
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/burger'> GitHub Repository </a>")
-            $("#deployed").html("<a href='https://peaceful-badlands-20443.herokuapp.com/'> Deployed Application </a>")
-
-            break;
-
-        case "bamazon":
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/bamazon_assignment'> GitHub Repository </a>")
-            $("#deployed").html("CLI App - Undeployable")
-
-            break;
-
-        case "wordGuess-adv":
-
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/word_guess_node'> GitHub Repository </a>")
-            $("#deployed").html("CLI App - Undeployable")
-
-            break;
-
-        case "liri":
-
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/liri-node-app'> GitHub Repository </a>")
-            $("#deployed").html("CLI App - Undeployable")
-
-            break;
-
-        case "trivia":
-
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/TriviaGame'> GitHub Repository </a>")
-            $("#deployed").html("<a href='https://hirohtk.github.io/TriviaGame/'> Deployed Application </a>")
-
-            break;
-
-        case "starWars":
-
-            $("#guide-modal").modal();
-            $("#projectTitle").text(specificAssignment);
-            $("#repo").html("<a href='https://github.com/hirohtk/Star-wars-game'> GitHub Repository </a>")
-            $("#deployed").html("<a href='https://hirohtk.github.io/Star-wars-game/'> Deployed Application </a>")
-
-            break;
-
-        case "wordGuess-easy":
+        }
 
         $("#guide-modal").modal();
         $("#projectTitle").text(specificAssignment);
-        $("#repo").html("<a href='https://github.com/hirohtk/Word-guess-game'> GitHub Repository </a>")
-        $("#deployed").html("<a href='https://hirohtk.github.io/Word-guess-game/'> Deployed Application </a>")
-
-        break;
-        
+        $("#projectTitle").attr("style", "font-weight: bold; display:inline;");
+        $("#repo").html("<a href='" + repo + "'> GitHub Repository </a>");
+        if (deployed.search("CLI") === -1) {
+            $("#deployed").html("<a href='" + deployed + "'> Deployed Application </a>");
+        }
+        else {
+            $("#deployed").html(deployed);
+        }
     }
 
+    dry1(specificAssignment);
 
 });
